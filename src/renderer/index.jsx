1a0ec996c4704ffaea36b53141d035371d0366a8
kistdeov <kistuttidavidka@gmail.com>
2025-10-01 15:51:56 +0200

    1.1.3

--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -1101,7 +1101,7 @@ const App = () => {
               onMouseLeave={handleDrawerMouseLeave}
             >
               <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', p: 1 }}>
-                <Typography variant='caption' sx={{ mr: 9 }}>Verzió: 1.1.2</Typography>
+                <Typography variant='caption' sx={{ mr: 9 }}>Verzió: 1.1.3</Typography>
                 <IconButton onClick={handlePinClick} size="small" color={isPinned ? 'error' : 'default'}>
                   {isPinned ? (
                     <FaTimesCircle size={20} color="#d32f2f" />
